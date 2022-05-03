a66fe23a2eb11a5ee3f2ff4291138926ee615fd3
giselleSoledad <gisol14@hotmail.com>
2022-05-02 22:40:10 -0300

    ok

---
+++ ejercicio18/asset/script16b.js
@@ -0,0 +1,14 @@
+//CREAR UN ALGORITMO JS SIMPLE
+//Formato: Código fuente en archivo JavaScript,
+//formato .js, vinculado al archivo HTML correspondiente.
+//Sugerencia: Usamos prompt() para solicitar datos
+//al usuario y console.log() o alert() para mostrar el
+//resultado de las operaciones realizadas con esos datos.
+//Si vas a sumar una entrada, tené en cuenta que tenés
+//que parsearla antes. Usando parseInt() o parseFloat() 
+//Consigna: Crea un script en JS que le solicite al
+//usuario ingresar mínimo 1(un) dato.
+ //Luego, con JavaScript, realiza operaciones
+//matemáticas o de concatenación sobre las entradas
+//teniendo en cuenta el tipo de dato. Al finalizar
+//mostrar el resultados con alert() o console.log() 
