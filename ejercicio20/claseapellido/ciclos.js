0975f4c89d88641bdcd061aaa3cce4e9629509f1
giselleSoledad <gisol14@hotmail.com>
2022-05-04 20:22:06 -0300

    cambio

---
+++ ejercicio20/claseapellido/ciclos.js
@@ -0,0 +1,14 @@
+//1. CREAR UN ALGORITMO UTILIZANDO UN CICLO
+//Tomando como base los ejemplos anteriores de la
+//estructura for, while y do...while, crear un algoritmo que
+//repita un bloque de instrucciones
+//>> Consigna: Tomando como base los ejemplos anteriores
+//de la estructura for y while, crear un algoritmo que repita un bloque
+//de instrucciones.
+//En cada repetición es necesario efectuar
+//una operación o comparación para obtener una salida por alerta o consola.
+//>>Aspectos a incluir en el entregable:
+//Archivo HTML y Archivo JS, referenciado en el HTML por etiqueta
+//<script src="js/miarchivo.js"></script>, que incluya la definición de
+//un algoritmo en JavaScript que emplee bucles e instrucciones condicionales.
+
